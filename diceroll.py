21341a3ae47c0e5b82e16ff260d018d7dc2b5ad5
Keithd2423 <144353516+Keithd2423@users.noreply.github.com>
2023-12-06 15:11:43 +0000

    message

---
+++ diceroll.py
@@ -0,0 +1,25 @@
+import random
+
+#creating a dice roll game that makes a player roll a dice vs a computer. Then selects a winner based on a higher number.
+greeting = input("would you like to roll the dice? [Y/N]").lower()
+
+if (greeting == "y"):
+    print("hello")
+elif (greeting == "n"):
+    print("Program ended")
+else:
+    print("type in a correct Y or N")
+    
+
+random.randint(1, 10)
+
+val Player_Roll
+
+val Comp_Roll
+
+
+#storing computer and player diceroll
+
+#Using logic to choose a winner
+
+#printing results
